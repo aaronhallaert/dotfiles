952503f619a18d0b02079ba6dae9d190d7695dd9
Aaron Hallaert <aaronhallaert@hotmail.com>
2020-09-21 00:31:56 +0200

    Fix install script for linux

--- vim/install_linux.sh
+++ vim/install_linux.sh
@@ -4,6 +4,6 @@ vim_config_file=~/.vimrc
 coc_config_file=~/.config/nvim/coc-settings.json
 
 if [ ! -L "$nvim_config_file" ]; then
-  ln -s .vimrc "$vim_config_file"
-  ln -s coc-settings.json "$coc_config_file"
+  ln -s ~/git/dotfiles/vim/.vimrc "$vim_config_file"
+  ln -s ~/git/dotfiles/vim/coc-settings.json "$coc_config_file"
 fi
