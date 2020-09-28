cd5f376555ce12cd0179b6dd5ac64174e019c3a6
Aaron Hallaert <aaron.hallaert@ugent.be>
2020-09-28 02:26:49 +0200

    Update install linux script for neovim

--- vim/install_linux.sh
+++ vim/install_linux.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 vim_config_file=~/.vimrc
+nvim_config_file=~/.config/nvim/init.vim
 coc_config_file=~/.config/nvim/coc-settings.json
 
-if [ ! -L "$nvim_config_file" ]; then
-  ln -s ~/dotfiles/vim/.vimrc "$vim_config_file"
-  ln -s ~/dotfiles/vim/coc-settings.json "$coc_config_file"
-fi
+mkdir ~/.config/nvim
+
+ln -s ~/dotfiles/vim/.vimrc "$vim_config_file"
+ln -s ~/dotfiles/vim/.vimrc "$nvim_config_file"
+ln -s ~/dotfiles/vim/coc-settings.json "$coc_config_file"
