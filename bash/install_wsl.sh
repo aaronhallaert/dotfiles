e630dc031de466702771b8673aa56a4f9d2fccb2
Aaron Hallaert <hallaertaaron@gmail.com>
2020-10-12 11:47:21 +0200

    Update for WSL

---
+++ bash/install_wsl.sh
@@ -0,0 +1,1 @@
+ln -s ~/dotfiles/bash/.wslbashrc ~/.bashrc
--- scripts/make_handout.sh
+++ scripts/make_handout.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pdfjam --nup 2x2 --frame true --noautoscale false --delta "0.2cm 6cm" --scale 0.90 $1 --outfile $2
+pdfjam --nup 2x2 --frame true --noautoscale false --delta "0.2cm 6cm" --scale 0.90 $1 --outfile $1
