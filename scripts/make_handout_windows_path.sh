147e5d446102ad4ee012cf8d4cc1987d07c036d7
Aaron Hallaert <hallaertaaron@gmail.com>
2021-02-03 06:31:12 +0100

    Update handout script

--- scripts/make_handout_windows_path.sh
+++ scripts/make_handout_windows_path.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 FILE=`wslpath "$1"`
-pdfjam --nup 2x2 --frame true --noautoscale false --delta "0.2cm 6cm" --scale 0.90 "$FILE" --outfile "$FILE"
+pdfjam --nup 1x3 --frame true --noautoscale false --delta "0cm 2.5cm" --offset "-3.5cm 0cm" --scale 0.85 "$FILE" --outfile "$FILE"
