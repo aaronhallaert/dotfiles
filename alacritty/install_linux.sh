3fbb66dbe271a261da4d555ae627f5a761540ed8
aaronhallaert <hallaertaaron@gmail.com>
2021-02-15 11:51:34 +0100

    audio update and zsh

---
+++ alacritty/install_linux.sh
@@ -0,0 +1,4 @@
+alacritty_config_file=~/.config/alacritty/alacritty.yml
+
+mkdir -p ~/.config/alacritty
+ln -s ~/dotfiles/alacritty/alacritty.yml "$alacritty_config_file"
--- zsh/install_linux.sh
+++ zsh/install_linux.sh
@@ -7,4 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # clone a powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo "Set THEME ~/.zshrc to powerlevel10k/powerlevel10k"
+rm ~/.zshrc
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
