0341810914fe9d5eff0bad21ccde74b0eb41437b
aaronhallaert <hallaertaaron@gmail.com>
2021-02-15 11:51:07 +0100

    audio update and zsh

--- scripts/desktop-4k-philipssquare.sh
+++ scripts/desktop-4k-philipssquare.sh
@@ -2,3 +2,7 @@
 xrandr --fb 6400x2160\
     --output DP-0 --mode 3840x2160\
     --output HDMI-0 --auto --scale 2x2 --pos 3840x0 --panning 2560x2048+3840+0
+
+
+kquitapp5 plasmashell
+kstart5 plasmashell
