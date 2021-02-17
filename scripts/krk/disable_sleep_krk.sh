818bd742dbdb8028e2e4b9a96c20d79e84987584
aaronhallaert <hallaertaaron@gmail.com>
2021-02-17 13:18:28 +0100

    update krk scripts

--- scripts/krk/disable_sleep_krk.sh
+++ scripts/krk/disable_sleep_krk.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+jack_control start
+pacmd set-default-sink "jack_out"
+
+echo "" > ~/dotfiles/scripts/krk/krk.log
 watch -n 1500 ~/dotfiles/scripts/krk/play_10hz_tone.sh
--- scripts/krk/play_10hz_tone.sh
+++ scripts/krk/play_10hz_tone.sh
@@ -1,1 +1,2 @@
+echo "$(date): Wake up monitor" >> ~/dotfiles/scripts/krk/krk.log
 aplay -d 1 ~/dotfiles/scripts/krk/10hz_tone.wav
