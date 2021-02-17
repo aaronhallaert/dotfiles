18e9d00f363c56af21bebcd285a1275b376d047a
aaronhallaert <hallaertaaron@gmail.com>
2021-02-17 13:25:50 +0100

    "Update log for krk scripts"

--- scripts/krk/disable_sleep_krk.sh
+++ scripts/krk/disable_sleep_krk.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
-jack_control start
-pacmd set-default-sink "jack_out"
-
+# reset logfile
 echo "" > ~/dotfiles/scripts/krk/krk.log
+
+sleep 15
+echo "change audio" >> ~/dotfiles/scripts/krk/krk.log
+jack_control start >> ~/dotfiles/scripts/krk/krk.log
+echo "wait for 5 seconds" >> ~/dotfiles/scripts/krk/krk.log
+sleep 5
+echo "Set default sink" >> ~/dotfiles/scripts/krk/krk.log
+pacmd set-default-sink "jack_out" >> ~/dotfiles/scripts/krk/krk.log
+
 watch -n 1500 ~/dotfiles/scripts/krk/play_10hz_tone.sh
