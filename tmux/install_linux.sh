c10e125ee2f5476261810cebeef11d00b386d238
Aaron Hallaert <hallaertaaron@gmail.com>
2021-02-23 22:41:56 +0100

    Update settings for tmux

---
+++ tmux/install_linux.sh
@@ -0,0 +1,3 @@
+tmux_config_file=~/.tmux.conf
+rm $tmux_config_file
+ln -s ~/dotfiles/tmux/tmux.conf "$tmux_config_file"
---
+++ tmux/session_setup/thesis.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+tmux new -s thesis -d
+tmux neww -t thesis: -n workspace -c ~/repos/Thesis/thesis_game_of_pirates
+tmux send-keys -t thesis:workspace "conda activate thesis" 'Enter'
+tmux send-keys -t thesis:workspace "clear" 'Enter'
+
+tmux neww -t thesis: -n dain -c ~/repos/Thesis/Dain-Interpolation-Attack
+tmux send-keys -t thesis:dain "conda activate dainapp" 'Enter'
+tmux send-keys -t thesis:dain "clear" 'Enter'
+
+tmux neww -t thesis: -n rife -c ~/repos/Thesis/RIFE-Interpolation-Attack
+tmux send-keys -t thesis:rife "conda activate rifeapp" 'Enter'
+tmux send-keys -t thesis:rife "clear" 'Enter'
+
+
+tmux send-keys -t thesis:1 "cd ~/repos/Thesis/thesis_game_of_pirates" 'Enter'
+tmux send-keys -t thesis:1 "clear" 'Enter'
+
+tmux a -t thesis:workspace
+
+#while [ "$#" -gt 0 ]; do
+    #curr=$1
+    #shift
+
+    #case "$curr" in
+        #"-w")
+            #echo "-w entered"
+            #tmux neww -t thesis: -n workspace -c ~/repos/Thesis/thesis_game_of_pirates
+            #tmux send-keys -t thesis:workspace "conda activate thesis" 'Enter'
+            #;;
+        #"-d")
+            #echo "-d entered"
+            #tmux neww -t thesis: -n dain -c ~/repos/Thesis/Dain-Interpolation-Attack
+            #tmux send-keys -t thesis:dain "conda activate dainapp" 'Enter'
+            #;;
+        #"-r")
+            #echo "-r entered"
+            #tmux neww -t thesis: -n rife -c ~/repos/Thesis/RIFE-Interpolation-Attack
+            #tmux send-keys -t thesis:rife "conda activate rifeapp" 'Enter'
+            #;;
+        #*) "Unavailable command ... $curr"
+    #esac
+#done
