800a667d45f6152dd85d5ff46ff0f17dc9576bf8
Aaron Hallaert <hallaertaaron@gmail.com>
2021-03-25 00:42:22 +0100

    Update install scripts

---
+++ vim/Installation/install_prerequisites.sh
@@ -0,0 +1,7 @@
+# install vimplug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo apt install nodejs -y
+sudo apt install npm -y
+pip install neovim
