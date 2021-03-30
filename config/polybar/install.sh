9ebad6fa9d62535e9626f5aec99f440ad4ecc292
Aaron Hallaert <hallaertaaron@gmail.com>
2021-03-30 10:11:03 +0200

    Fix polybar install

--- config/polybar/install.sh
+++ config/polybar/install.sh
@@ -1,2 +1,4 @@
 rm ~/.config/polybar/config
+rm ~/.config/polybar/launch.sh
 ln -s ~/dotfiles/config/polybar/config ~/.config/polybar/config
+ln -s ~/dotfiles/config/polybar/launch.sh ~/.config/polybar/launch.sh
