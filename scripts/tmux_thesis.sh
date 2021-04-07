ec3a7f0d9341dec923c22640d236c821cdd6ff50
Aaron Hallaert <hallaertaaron@gmal.com>
2021-04-07 13:51:29 +0200

    Automate tmux session startup and attach

--- scripts/tmux_thesis.sh
+++ scripts/tmux_thesis.sh
@@ -1,42 +1,27 @@
 #!/bin/bash
-tmux new -s thesis -d
+session="thesis"
 
-tmux neww -t thesis: -n scriptie -c ~/repos/Thesis/thesis_scriptie_game_of_pirates/boek
-tmux send-keys -t thesis:scriptie "nvim -S ~/.config/nvim/sessions/thesis.vim" 'Enter'
+tmux has-session -t $session 2>/dev/null
 
-tmux neww -t thesis: -n workspace -c ~/repos/Thesis/thesis_game_of_pirates
-tmux send-keys -t thesis:workspace "conda activate thesis" 'Enter'
-tmux send-keys -t thesis:workspace "clear" 'Enter'
+if [ $? != 0 ]; then
 
-tmux neww -t thesis: -n rife -c ~/repos/Thesis/RIFE-Interpolation-Attack
-tmux send-keys -t thesis:rife "conda activate rifeapp" 'Enter'
-tmux send-keys -t thesis:rife "clear" 'Enter'
+    tmux new -s $session -d
 
-tmux send-keys -t thesis:1 "cd ~/repos/Thesis/thesis_game_of_pirates" 'Enter'
-tmux send-keys -t thesis:1 "clear" 'Enter'
+    tmux neww -t $session: -n scriptie -c ~/repos/Thesis/thesis_scriptie_game_of_pirates/boek
+    tmux send-keys -t $session:scriptie "nvim -S ~/.config/nvim/sessions/thesis.vim" 'Enter'
 
-tmux a -t thesis:scriptie
+    tmux neww -t $session: -n workspace -c ~/repos/Thesis/thesis_game_of_pirates
+    tmux send-keys -t $session:workspace "conda activate thesis" 'Enter'
+    tmux send-keys -t $session:workspace "clear" 'Enter'
 
-#while [ "$#" -gt 0 ]; do
-    #curr=$1
-    #shift
+    tmux neww -t $session: -n rife -c ~/repos/Thesis/RIFE-Interpolation-Attack
+    tmux send-keys -t $session:rife "conda activate rifeapp" 'Enter'
+    tmux send-keys -t $session:rife "clear" 'Enter'
 
-    #case "$curr" in
-        #"-w")
-            #echo "-w entered"
-            #tmux neww -t thesis: -n workspace -c ~/repos/Thesis/thesis_game_of_pirates
-            #tmux send-keys -t thesis:workspace "conda activate thesis" 'Enter'
-            #;;
-        #"-d")
-            #echo "-d entered"
-            #tmux neww -t thesis: -n dain -c ~/repos/Thesis/Dain-Interpolation-Attack
-            #tmux send-keys -t thesis:dain "conda activate dainapp" 'Enter'
-            #;;
-        #"-r")
-            #echo "-r entered"
-            #tmux neww -t thesis: -n rife -c ~/repos/Thesis/RIFE-Interpolation-Attack
-            #tmux send-keys -t thesis:rife "conda activate rifeapp" 'Enter'
-            #;;
-        #*) "Unavailable command ... $curr"
-    #esac
-#done
+    tmux send-keys -t $session:1 "cd ~/repos/Thesis/thesis_game_of_pirates" 'Enter'
+    tmux send-keys -t $session:1 "clear" 'Enter'
+
+fi
+
+tmux a -t $session:scriptie
+
