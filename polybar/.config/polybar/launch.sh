86b3cc251ff5a3074e9357c1e9fe6168b25cd80a
Aaron Hallaert <hallaertaaron@gmail.com>
2021-10-22 11:14:08 +0200

    Update i3 and polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,8 +5,17 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
