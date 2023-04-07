5caed04f1bec35617c3d99b6d5080f3482b1ca01
Aaron Hallaert <hallaertaaron@gmail.com>
2023-04-07 22:43:42 +0200

    Remove chatgpt plugin
    Signed-off-by: Aaron Hallaert <hallaertaaron@gmail.com>

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,22 +1,6 @@
 return {
     "aduros/ai.vim",
     {
-        "jackMort/ChatGPT.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("chatgpt").setup({
-                keymaps = {
-                    submit = "<C-s>",
-                },
-            })
-        end,
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
-        },
-    },
-    {
         "voldikss/vim-floaterm",
         config = function()
             vim.api.nvim_set_keymap(
