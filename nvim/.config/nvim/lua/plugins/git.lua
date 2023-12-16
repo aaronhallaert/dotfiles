3b2b0b42ee7cdf4b8b8db68135c5c0a31efcd841
Aaron Hallaert <hallaertaaron@gmail.com>
2023-12-16 13:46:26 +0100

    Enable gitlinker
    Signed-off-by: Aaron Hallaert <hallaertaaron@gmail.com>

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -32,22 +32,22 @@ return {
         },
         event = "VeryLazy",
     },
-    -- {
-    --     "ruifm/gitlinker.nvim",
-    --     dependencies = { "nvim-lua/plenary.nvim" },
-    --     config = function()
-    --         require("gitlinker").setup({
-    --             opts = {
-    --                 add_current_line_on_normal_mode = false,
-    --                 action_callback = function(url)
-    --                     require("gitlinker.actions").copy_to_clipboard(url)
-    --                     require("gitlinker.actions").open_in_browser(url)
-    --                 end,
-    --             },
-    --         })
-    --     end,
-    --     event = "VeryLazy",
-    -- },
+    {
+        "ruifm/gitlinker.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("gitlinker").setup({
+                opts = {
+                    add_current_line_on_normal_mode = false,
+                    action_callback = function(url)
+                        require("gitlinker.actions").copy_to_clipboard(url)
+                        require("gitlinker.actions").open_in_browser(url)
+                    end,
+                },
+            })
+        end,
+        event = "VeryLazy",
+    },
     {
         "pwntester/octo.nvim",
         dependencies = {
