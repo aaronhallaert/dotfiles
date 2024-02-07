1698ad0940db89bdfc286b9c035220e6f23bca37
Aaron Hallaert <hallaertaaron@gmail.com>
2024-02-07 09:32:17 +0100

    feat: scroll completions in search with c-p and c-n

--- nvim/.config/nvim/lua/plugins/config/cmp.lua
+++ nvim/.config/nvim/lua/plugins/config/cmp.lua
@@ -83,7 +83,10 @@ cmp.setup({
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", { sources = { { name = "buffer" } } })
+cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = { { name = "buffer" } },
+})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
