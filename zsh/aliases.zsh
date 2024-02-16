0806bc6d4254f040533edb13171a1d5fab54e1d1
Aaron Hallaert <hallaertaaron@gmail.com>
2024-02-16 09:30:58 +0100

    feat: add vpn aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,18 +5,18 @@ alias dotf="cd ~/dotfiles"
 alias lg="lazygit"
 alias ld="lazydocker"
 alias vi="nvim --listen /tmp/\$(tmux display-message -p '#S-#{window_index}-#I')"
-alias cat="bat"
+# alias cat="bat"
 alias gp="git pull"
 alias gP="git push"
 alias git commit="git commit -S"
 alias wip="git commit -m 'wip' --no-verify"
 alias nipro="tmux_nipro.sh"
-alias nipro-repo="cd ~/Developer/nephroflow/\$(ls ~/Developer/nephroflow/ | fzf)"
 alias plugins="cd ~/.local/share/nvim/lazy/"
 
 alias devup="devcontainer up --workspace-folder ./"
 
-
+alias vpnu="openvpn3 session-start --config ~/Documents/televic.ovpn"
+alias vpnd="openvpn3 session-manage --disconnect --path \$(openvpn3 sessions-list | grep Path | cut -d ':' -f2 | tr -d ' ')"
 
 alias dcd="docker compose down"
 alias dcu="docker compose up"
