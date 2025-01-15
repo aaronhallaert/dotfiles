d7a0a8ee318fe042536c086fa67cdb9e9efaa927
Aaron Hallaert <hallaertaaron@gmail.com>
2025-01-15 09:29:39 +0100

    feat(sway): block/unblock bluetooth on sleep

--- sway/.config/sway/scripts/swayidle.sh
+++ sway/.config/sway/scripts/swayidle.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+BTSTOP="/usr/sbin/rfkill block bluetooth"
+BTSTART="/usr/sbin/rfkill unblock bluetooth"
 PAUSE="playerctl -a pause"
 LOCK="swaylock -f -i ~/lockscreen_wallpaper.jpg"
+
+RECONNECT_BT_DEVICES="bluetoothctl connect D7:9B:C4:84:02:12"
 
 killall swayidle 
 wait
@@ -13,4 +17,5 @@ swayidle -d -w \
          resume 'swaymsg "output * dpms on"' \
     timeout 600 'systemctl suspend' \
          resume 'swaymsg "output * dpms on"' \
-    before-sleep "$PAUSE ; $LOCK" > ~/tmp/swayidle.log
+    before-sleep "$PAUSE ; $BTSTOP ; $LOCK;" \
+    after-resume "$BTSTART; $RECONNECT_BT_DEVICES" > ~/tmp/swayidle.log
