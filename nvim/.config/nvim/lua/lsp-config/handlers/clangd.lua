4a66a4904c9304fb58bf15daf09556982820f18f
Aaron Hallaert <hallaertaaron@gmail.com>
2025-10-09 13:50:37 +0200

    fix(nvim): refine clangd query-driver path to build_scripts toolchains

--- nvim/.config/nvim/lua/lsp-config/handlers/clangd.lua
+++ nvim/.config/nvim/lua/lsp-config/handlers/clangd.lua
@@ -1,27 +1,17 @@
 local M = {}
 vim.lsp.enable("clangd", false)
 
-M.setup = function(nvim_lsp)
+M.setup = function()
     local cmd = {
         "clangd",
-        -- "--query-driver="
-        --     .. vim.fn.expand("$HOME")
-        --     .. "/Developer/televic/build_scripts/toolchains/televic_pc_sdk_2023.02.9/bin/i686-linux-g++",
-        -- "--query-driver="
-        --     .. vim.fn.expand("$HOME")
-        --     .. "/Developer/televic/build_scripts_6/toolchains/mmu-glibc-x86_64-cortexa9hf-neon-toolchain-6.6.x/sysroots/x86_64-tcssdk-linux/usr/bin/arm-tcs-linux-gnueabi/arm-tcs-linux-gnueabi-g++",
-        -- "--query-driver="
-        --     .. vim.fn.expand("$HOME")
-        --     .. "/.local/share/build_scripts/**/*g++",
         "--query-driver="
             .. vim.fn.expand("$HOME")
-            .. "/**/*g++",
-
+            .. "/.local/share/build_scripts/toolchains/**/*g++",
         "--clang-tidy=false",
     }
 
-    nvim_lsp.clangd.setup({
-        root_dir = nvim_lsp.util.root_pattern("CMakeLists.txt"),
+    vim.lsp.config.clangd = {
+        root_markers = {"CMakeLists.txt"},
         single_file_support = true,
         capabilities = {
             textDocument = {
@@ -38,7 +28,8 @@ M.setup = function(nvim_lsp)
             end
         end,
         cmd = cmd,
-    })
+        arg = {},
+    }
 end
 
 return M
