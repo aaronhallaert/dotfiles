37b7a33bc10ec369996b98cb057935b255daf07e
Aaron Hallaert <hallaertaaron@gmail.com>
2026-01-20 12:00:20 +0100

    feat(sway): swayidle hopefully correct now

--- sway/.config/sway/scripts/swayidle.sh
+++ sway/.config/sway/scripts/swayidle.sh
@@ -5,19 +5,20 @@ BTSTART="/usr/sbin/rfkill unblock bluetooth"
 PAUSE="playerctl -a pause"
 LOCK="swaylock -f -i ~/lockscreen_wallpaper.jpg"
 
-KANSHI_RELOAD="kanshi --reload"
+KANSHI_RELOAD="kanshi reload"
 RECONNECT_BT_DEVICES="bluetoothctl connect D7:9B:C4:84:02:12"
+
+SLEEP="systemctl suspend"
+SCREEN_OFF="swaymsg 'output * dpms off'"
+SCREEN_ON="swaymsg 'output * dpms on'"
 
 killall swayidle 
 wait
 
-swayidle -d -w \
-    timeout 299 "exec $PAUSE" \
-    timeout 300 "exec $LOCK" \
-    timeout 301 'swaymsg "output * dpms off"' \
-         resume 'swaymsg "output * dpms on"' \
-    timeout 600 'systemctl suspend' \
-         resume 'swaymsg "output * dpms on"' \
-    before-sleep "$PAUSE ; $LOCK;" \
-    after-resume "$KANSHI_RELOAD" \
-    after-resume "$RECONNECT_BT_DEVICES" > ~/tmp/swayidle.log
+swayidle -d \
+    timeout 250 "${SCREEN_OFF}" \
+        resume "${SCREEN_ON}" \
+    timeout 300 "${SLEEP}" \
+    before-sleep "${PAUSE}" \
+    before-sleep "${LOCK}" \
+    after-resume "${KANSHI_RELOAD}"
