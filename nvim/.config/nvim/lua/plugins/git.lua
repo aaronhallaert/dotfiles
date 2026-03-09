8bb0c72ccb5281502ee9cd23719e77c7c85a56a3
Aaron Hallaert <hallaertaaron@gmail.com>
2026-03-09 09:55:59 +0100

    feat(nvim): add neogit plugin

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -10,6 +10,27 @@ return {
         end,
     },
     {
+        "NeogitOrg/neogit",
+        lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+
+            -- Only one of these is needed.
+            "sindrets/diffview.nvim", -- optional
+            "esmuellert/codediff.nvim", -- optional
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua", -- optional
+            "nvim-mini/mini.pick", -- optional
+            "folke/snacks.nvim", -- optional
+        },
+        cmd = "Neogit",
+        keys = {
+            { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+        },
+    },
+    {
         "aaronhallaert/advanced-git-search.nvim",
         dev = true,
         cmd = { "AdvancedGitSearch" },
